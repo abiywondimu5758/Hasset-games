2a8c59c003870fad3d86110a1133e90c8eff56e5
abiywondimu5758 <abiywondimu1@gmail.com>
2025-06-20 16:21:57 +0300

    URL update

--- game_platform_frontend/src/helper/Api.js
+++ game_platform_frontend/src/helper/Api.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
-const BASE_URL = "https://api.bingo.fortunebets.net";
-export const socket = io("https://api.bingo.fortunebets.net");
+const BASE_URL = "https://api.hasetgames.com";
+export const socket = io("https://api.hasetgames.com");
+
 
 // const BASE_URL = "http://localhost:5000";
 // export const socket = io("http://localhost:5000");
